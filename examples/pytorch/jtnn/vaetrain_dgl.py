ab45893b718427f5da0d3e8400e64acb8129d10e
VoVAllen <jz1749@nyu.edu>
2019-08-05 09:49:45 +0000

    :new:

--- examples/pytorch/jtnn/vaetrain_dgl.py
+++ examples/pytorch/jtnn/vaetrain_dgl.py
@@ -4,23 +4,30 @@ import torch.optim as optim
 import torch.optim.lr_scheduler as lr_scheduler
 from torch.utils.data import DataLoader
 
-import math, random, sys
+import math
+import random
+import sys
 from optparse import OptionParser
 from collections import deque
 import rdkit
 
 from jtnn import *
 
-torch.multiprocessing.set_sharing_strategy('file_system')
+# torch.multiprocessing.set_sharing_strategy('file_system')
+
 
 def worker_init_fn(id_):
-    lg = rdkit.RDLogger.logger() 
+    lg = rdkit.RDLogger.logger()
     lg.setLevel(rdkit.RDLogger.CRITICAL)
+
+
 worker_init_fn(None)
 
 parser = OptionParser()
-parser.add_option("-t", "--train", dest="train", default='train', help='Training file name')
-parser.add_option("-v", "--vocab", dest="vocab", default='vocab', help='Vocab file name')
+parser.add_option("-t", "--train", dest="train",
+                  default='train', help='Training file name')
+parser.add_option("-v", "--vocab", dest="vocab",
+                  default='vocab', help='Vocab file name')
 parser.add_option("-s", "--save_dir", dest="save_path")
 parser.add_option("-m", "--model", dest="model_path", default=None)
 parser.add_option("-b", "--batch", dest="batch_size", default=40)
@@ -30,8 +37,9 @@ parser.add_option("-d", "--depth", dest="depth", default=3)
 parser.add_option("-z", "--beta", dest="beta", default=1.0)
 parser.add_option("-q", "--lr", dest="lr", default=1e-3)
 parser.add_option("-T", "--test", dest="test", action="store_true")
-opts,args = parser.parse_args()
+opts, args = parser.parse_args()
 
+print(opts)
 dataset = JTNNDataset(data=opts.train, vocab=opts.vocab, training=True)
 vocab = dataset.vocab
 
@@ -54,7 +62,8 @@ else:
             nn.init.xavier_normal(param)
 
 model = cuda(model)
-print("Model #Params: %dK" % (sum([x.nelement() for x in model.parameters()]) / 1000,))
+print("Model #Params: %dK" %
+      (sum([x.nelement() for x in model.parameters()]) / 1000,))
 
 optimizer = optim.Adam(model.parameters(), lr=lr)
 scheduler = lr_scheduler.ExponentialLR(optimizer, 0.9)
@@ -63,19 +72,20 @@ scheduler.step()
 MAX_EPOCH = 100
 PRINT_ITER = 20
 
+
 def train():
     dataset.training = True
     dataloader = DataLoader(
-            dataset,
-            batch_size=batch_size,
-            shuffle=True,
-            num_workers=4,
-            collate_fn=JTNNCollator(vocab, True),
-            drop_last=True,
-            worker_init_fn=worker_init_fn)
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=4,
+        collate_fn=JTNNCollator(vocab, True),
+        drop_last=True,
+        worker_init_fn=worker_init_fn)
 
     for epoch in range(MAX_EPOCH):
-        word_acc,topo_acc,assm_acc,steo_acc = 0,0,0,0
+        word_acc, topo_acc, assm_acc, steo_acc = 0, 0, 0, 0
 
         for it, batch in enumerate(dataloader):
             model.zero_grad()
@@ -100,10 +110,10 @@ def train():
 
                 print("KL: %.1f, Word: %.2f, Topo: %.2f, Assm: %.2f, Steo: %.2f, Loss: %.6f" % (
                     kl_div, word_acc, topo_acc, assm_acc, steo_acc, loss.item()))
-                word_acc,topo_acc,assm_acc,steo_acc = 0,0,0,0
+                word_acc, topo_acc, assm_acc, steo_acc = 0, 0, 0, 0
                 sys.stdout.flush()
 
-            if (it + 1) % 1500 == 0: #Fast annealing
+            if (it + 1) % 1500 == 0:  # Fast annealing
                 scheduler.step()
                 print("learning rate: %.6f" % scheduler.get_lr()[0])
                 torch.save(model.state_dict(),
@@ -111,18 +121,20 @@ def train():
 
         scheduler.step()
         print("learning rate: %.6f" % scheduler.get_lr()[0])
-        torch.save(model.state_dict(), opts.save_path + "/model.iter-" + str(epoch))
+        torch.save(model.state_dict(), opts.save_path +
+                   "/model.iter-" + str(epoch))
+
 
 def test():
     dataset.training = False
     dataloader = DataLoader(
-            dataset,
-            batch_size=1,
-            shuffle=False,
-            num_workers=0,
-            collate_fn=JTNNCollator(vocab, False),
-            drop_last=True,
-            worker_init_fn=worker_init_fn)
+        dataset,
+        batch_size=1,
+        shuffle=False,
+        num_workers=0,
+        collate_fn=JTNNCollator(vocab, False),
+        drop_last=True,
+        worker_init_fn=worker_init_fn)
 
     # Just an example of molecule decoding; in reality you may want to sample
     # tree and molecule vectors.
@@ -135,6 +147,7 @@ def test():
         smiles = model.decode(tree_vec, mol_vec)
         print(smiles)
 
+
 if __name__ == '__main__':
     if opts.test:
         test()
@@ -146,10 +159,12 @@ if __name__ == '__main__':
     print('Total # edges processed:', model.n_edges_total)
     print('Total # tree nodes processed:', model.n_tree_nodes_total)
     print('Graph decoder: # passes:', model.jtmpn.n_passes)
-    print('Graph decoder: Total # candidates processed:', model.jtmpn.n_samples_total)
+    print('Graph decoder: Total # candidates processed:',
+          model.jtmpn.n_samples_total)
     print('Graph decoder: Total # nodes processed:', model.jtmpn.n_nodes_total)
     print('Graph decoder: Total # edges processed:', model.jtmpn.n_edges_total)
     print('Graph encoder: # passes:', model.mpn.n_passes)
-    print('Graph encoder: Total # candidates processed:', model.mpn.n_samples_total)
+    print('Graph encoder: Total # candidates processed:',
+          model.mpn.n_samples_total)
     print('Graph encoder: Total # nodes processed:', model.mpn.n_nodes_total)
     print('Graph encoder: Total # edges processed:', model.mpn.n_edges_total)

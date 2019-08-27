1665ff4a1a7ade6a0b82fe579603342eac8e5291
VoVAllen <jz1749@nyu.edu>
2019-08-27 17:10:23 +0000

    Change to nn module

--- python/dgl/model_zoo/chem/mpnn.py
+++ python/dgl/model_zoo/chem/mpnn.py
@@ -9,83 +9,7 @@ import torch.nn.functional as F
 from torch.nn import Parameter
 import dgl.function as fn
 import dgl.nn.pytorch as dgl_nn
-
-
-class NNConvLayer(nn.Module):
-    """
-    MPNN Conv Layer from Section.5 in the paper "Neural Message Passing for Quantum Chemistry."
-    """
-
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 edge_net,
-                 root_weight=True,
-                 bias=True):
-        """
-        Args:
-            in_channels: number of input channels
-            out_channels: number of output channels
-            edge_net: the network modules process the edge info
-            root_weight: whether add the root node feature to output
-            bias: whether add bias to the output
-        """
-
-        super().__init__()
-
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.edge_net = edge_net
-
-        if root_weight:
-            self.root = Parameter(torch.Tensor(in_channels, out_channels))
-        else:
-            self.register_parameter('root', None)
-
-        if bias:
-            self.bias = Parameter(torch.Tensor(out_channels))
-        else:
-            self.register_parameter('bias', None)
-
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        if self.root is not None:
-            nn.init.xavier_normal_(self.root.data, gain=1.414)
-        if self.bias is not None:
-            self.bias.data.zero_()
-        for m in self.edge_net.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_normal_(m.weight.data, gain=1.414)
-
-    def message(self, edges):
-        return {
-            'm':
-            torch.matmul(edges.src['h'].unsqueeze(1),
-                         edges.data['w']).squeeze(1)
-        }
-
-    def apply_node_func(self, nodes):
-        aggr_out = nodes.data['aggr_out']
-        if self.root is not None:
-            aggr_out = torch.mm(nodes.data['h'], self.root) + aggr_out
-
-        if self.bias is not None:
-            aggr_out = aggr_out + self.bias
-
-        return {'h': aggr_out}
-
-    def forward(self, g, h, e):
-        """MPNN Conv layer forward."""
-        h = h.unsqueeze(-1) if h.dim() == 1 else h
-        e = e.unsqueeze(-1) if e.dim() == 1 else e
-
-        g.ndata['h'] = h
-        g.edata['w'] = self.edge_net(e).view(-1, self.in_channels,
-                                             self.out_channels)
-        g.update_all(self.message, fn.sum("m", "aggr_out"),
-                     self.apply_node_func)
-        return g.ndata.pop('h')
+from dgl.nn.pytorch import NNConv
 
 
 class MPNNModel(nn.Module):
@@ -124,10 +48,12 @@ class MPNNModel(nn.Module):
         edge_network = nn.Sequential(
             nn.Linear(edge_input_dim, edge_hidden_dim), nn.ReLU(),
             nn.Linear(edge_hidden_dim, node_hidden_dim * node_hidden_dim))
-        self.conv = NNConvLayer(in_channels=node_hidden_dim,
-                                out_channels=node_hidden_dim,
-                                edge_net=edge_network,
-                                root_weight=False)
+        self.conv = NNConv(in_feats=node_hidden_dim,
+                           out_feats=node_hidden_dim,
+                           edge_func=edge_network,
+                           aggregator_type="sum",
+                           residual=False,
+                           bias=True)
         self.gru = nn.GRU(node_hidden_dim, node_hidden_dim)
 
         self.set2set = dgl_nn.glob.Set2Set(node_hidden_dim, num_step_set2set,

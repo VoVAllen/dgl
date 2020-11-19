d18318584318a00a09acbec4488cd2a2730d056c
VoVAllen <jz1749@nyu.edu>
2020-11-19 10:27:01 +0000

    1

--- src/rpc/network/fabric/fabric_receiver.cc
+++ src/rpc/network/fabric/fabric_receiver.cc
@@ -33,7 +33,7 @@ bool FabricReceiver::Wait(const char* addr, int num_sender) {
   {
     // can be optimized with buffer pool
     // Will be freed in HandleCompletionEvent
-    int64_t* size_buffer = new int64_t;
+    int64_t* size_buffer = new int64_t(-99);
     // Issue recv events
     fep->Recv(size_buffer, sizeof(int64_t), kSizeMsg, FI_ADDR_UNSPEC, false,
               0xFFFFFFFF0000FFFF);
